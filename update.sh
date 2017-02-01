fcd3ae715c21dc0d006b26480836b3ca3288c63a
musicmancorley <musicmancorley@gmail.com>
2017-01-31 21:12:25 -0500

    checking for sendmail service at login

--- update.sh
+++ update.sh
@@ -2,12 +2,20 @@
 echo "Checking for updates..."
 git remote show origin | grep "out of date"
 if [ $? == 0 ] ; then
-   echo "An update is available! Updating now!"
+   echo "<b style='background:green'>An update is available! Updating now!</b><br>"
    git pull
    exit
    else
-   echo "All good here.  No updates available at this time"
+   echo "<b>All good here.  No updates available at this time</b><br>"
    fi
+
+ps aux | grep sendmail | grep "accepting connections" &>/dev/nul
+
+if [ $? == 0 ] ; then
+echo "<br>Checking if sendmail service is running: <b>Sendmail service is running!</b>"
+else
+echo "<br>Checking if sendmail service is running: <b style='background:red'>Sendmail service is NOT running</b>"
+fi
 
 grep -q broretention.sh /etc/sudoers
 if [ $? == 0 ] ; then
@@ -19,3 +27,4 @@ grep -q TheBriarPatch /etc/sudoers
 if [ $? != 0 ] ; then
 sudo sed -i -e '$awww-data ALL = (root) NOPASSWD: /var/www/html/TheBriarPatch/' /etc/sudoers
 fi
+
