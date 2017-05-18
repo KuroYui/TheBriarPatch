f3e358ac650ec231b9d162abbfcf2816f3784a7a
musicmancorley <musicmancorley@gmail.com>
2017-05-18 14:43:39 -0400

    more updates...

--- archiveddata.sh
+++ archiveddata.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [ "$2" == "Apple" ]; then
+grep $1 /var/log/suricata/http.log | grep -e 'iPhone' -e 'Macintosh' -e 'iPad' >> $3.txt
+else if [ "$2" == "Linux" ]; then
+grep $1 /var/log/suricata/http.log | grep -e "Linux" -e "CrOS" >> $3.txt
+else
 grep $1 /var/log/suricata/http.log | grep "$2" >> $3.txt
+fi
+fi
